2709fc70ecb4ac5f19b2833a5dce5b3163b4a230
anaesilva <annadasilva89@hotmail.com>
2024-07-28 17:27:36 -0300

    Predicate interface implementation

---
+++ Product/src/main/java/entities/Product.java
@@ -0,0 +1,14 @@
+package entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Product {
+    private String name;
+    private Double price;
+}
