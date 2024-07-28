66a08757df4167e38d2c41eed183705ff368b54f
anaesilva <annadasilva89@hotmail.com>
2024-07-28 18:48:39 -0300

    Interface implementation

---
+++ Consumer/src/main/java/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import entities.Product;
+import util.ProductConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+//        Fazer um programa que, a partir de uma lista de produtos, aumente o
+//        preço dos produtos em 10%.
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(new ProductConsumer());
+
+        list.forEach(System.out::println);
+    }
+}
---
+++ Consumer/src/main/java/entities/Product.java
@@ -0,0 +1,21 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+    private String name;
+    private Double price;
+
+    @Override
+    public String toString() {
+        return name + ", " + String.format("%.2f", price);
+    }
+}
