5b6fa8b729bb26b7a7f0fec6fc661a006eb688ca
anaesilva <annadasilva89@hotmail.com>
2024-07-28 17:37:21 -0300

    Method reference static

--- Predicate/src/main/java/application/Main.java
+++ Predicate/src/main/java/application/Main.java
@@ -1,13 +1,14 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("Tv", 900.00));
@@ -15,7 +16,7 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(new ProductPredicate());
+        list.removeIf(Product::staticProductPredicate);
 
         for (Product p : list) {
             System.out.println(p);
--- Predicate/src/main/java/entities/Product.java
+++ Predicate/src/main/java/entities/Product.java
@@ -14,6 +14,10 @@ public class Product {
     private String name;
     private Double price;
 
+    public static boolean staticProductPredicate(Product p) {
+        return p.getPrice() >= 100.0;
+    }
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
