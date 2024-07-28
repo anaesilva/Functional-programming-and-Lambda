29eff68e17ca89e34b15dd063f0b2fed291fe912
anaesilva <annadasilva89@hotmail.com>
2024-07-28 18:50:45 -0300

    Method reference static

--- Consumer/src/main/java/entities/Product.java
+++ Consumer/src/main/java/entities/Product.java
@@ -14,6 +14,10 @@ public class Product {
     private String name;
     private Double price;
 
+    public static void staticPriceUpdate(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
